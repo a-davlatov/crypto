11806bdf20f0e801385516ee1225110500774823
unknown <davlatovamriddin7@gmail.com>
2024-11-20 15:49:55 +0500

    adding dist

---
+++ assets/index-C8ixjve7.js
@@ -0,0 +1,1 @@
+import{aX as n,cz as o,cA as s}from"./index-DYXwBPv-.js";const a=n(o),t=n(s);export{t as _,a};
---
+++ assets/useErrorImage-CZMx-GP2.js
@@ -0,0 +1,1 @@
+const a=r=>t=>{t.target.src=r[Math.floor(Math.random()*r.length)]};export{a as u};
